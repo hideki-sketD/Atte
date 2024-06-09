90b49e60672f712fc9e30e544812aac376c6a674
tanaka hideki <kakifly2929@icloud.com>
2024-06-09 18:03:19 +0900

    流れ作成” git commit -m 流れ作成”

--- atte/src/resources/views/layouts/app.blade.php
+++ atte/src/resources/views/layouts/app.blade.php
@@ -13,28 +13,31 @@
 
 <body>
   <header class="header">
-    <div class="header__inner">
-      <div class="header-utilities">
-        <a class="header__logo" href="/attendance">
-          日付一覧
-        </a>
-        <nav>
-          <ul class="header-nav">
+      <h1 class="header-ttl">
+        Atte
+      </h1>
+        <nav class="header-nav">
+          <ul class="header-nav-list">
             @if (Auth::check())
             <li class="header-nav__item">
-              <a class="header-nav__link" href="/">ホーム</a>
+              <a class="header-nav__link" href="/">ホーム
+              </a>
+            </li>
+            <li class="header-nav__item">
+              <a class="header__logo" href="/attendance">
+              日付一覧
+              </a>
             </li>
             <li class="header-nav__item">
               <form class="form" action="/logout" method="post">
                 @csrf
-                <button class="header-nav__button">ログアウト</button>
+                <button class="header-nav__button">ログアウト
+                </button>
               </form>
             </li>
             @endif
           </ul>
         </nav>
-      </div>
-    </div>
   </header>
 
   <main>
